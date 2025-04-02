c97a4f3ec068688e7529ec45497a664b664c0ad3
mferradans <mferradanss@gmail.com>
2025-04-02 13:06:20 -0300

    success

--- src/components/SuccessPage.jsx
+++ src/components/SuccessPage.jsx
@@ -3,43 +3,45 @@ import { useLocation, useNavigate } from 'react-router-dom';
 import { Button, Container, Alert } from 'react-bootstrap';
 import Confetti from 'react-confetti'; // Solo si decides agregar confeti
 import { useWindowSize } from 'react-use'; // Para manejar el tamaño de la ventana
-
 const SuccessPage = () => {
   const location = useLocation();
+  console.log("✅ SuccessPage cargado");
+  console.log("🌍 URL actual:", window.location.href);
+
+  const queryParams = new URLSearchParams(location.search);
+  console.log("🔍 Query Params:", location.search);
+
+  // Ajustar aquí para usar 'external_reference' como el transactionId
+  const transactionId = queryParams.get('external_reference');
+  console.log("🆔 Transaction ID obtenido:", transactionId);
+
+  const navigate = useNavigate();
+  const API_URL = import.meta.env.VITE_BACKEND_URL;
+
+  // Obtener el tamaño de la ventana para el confeti
   const { width, height } = useWindowSize();
 
-  useEffect(() => {
-    console.log("✅ SuccessPage cargado");
-    console.log("🌍 URL actual:", window.location.href);
-
-    const queryParams = new URLSearchParams(location.search);
-    console.log("🔍 Query Params:", location.search);
-
-    const transactionId = queryParams.get('transactionId');
-    console.log("🆔 Transaction ID obtenido:", transactionId);
-  }, [location]);
-
-  const handleDownload = (transactionId) => {
+  const handleDownload = () => {
     if (!transactionId) {
       console.error("❌ Error: No se recibió transactionId en la URL.");
       return;
     }
-    const API_URL = import.meta.env.VITE_BACKEND_URL;
     console.log(`📥 Intentando descargar comprobante desde: ${API_URL}/download_receipt/${transactionId}`);
     window.open(`${API_URL}/download_receipt/${transactionId}`, '_blank');
   };
 
-  const transactionId = new URLSearchParams(location.search).get('transactionId');
-
   return (
     <Container className="text-center mt-5">
+      {/* Mostrar confeti solo si se quiere agregar */}
       <Confetti width={width} height={height} numberOfPieces={150} />
+
       <Alert variant="success">
         <h1>¡Pago exitoso!</h1>
         <p>Gracias por tu compra. Descarga este comprobante y preséntalo el día del evento ¡no lo olvides!</p>
       </Alert>
+
       {transactionId ? (
-        <Button variant="success" size="lg" onClick={() => handleDownload(transactionId)} className="my-3">
+        <Button variant="success" size="lg" onClick={handleDownload} className="my-3">
           Descargar Comprobante
         </Button>
       ) : (
@@ -50,3 +52,4 @@ const SuccessPage = () => {
 };
 
 export default SuccessPage;
+
